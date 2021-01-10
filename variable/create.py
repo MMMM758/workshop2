f3d6d0335723599ca4f262e82ba47cb605e84473
MMMM758 <aristamorgan87@gmail.com>
2021-01-10 14:16:48 +0700

    add create getType and single or dubble

---
+++ variable/create.py
@@ -0,0 +1,14 @@
+name = "Atitaya Arsawang"
+age = 20
+weight = 80.0
+can_walk = True
+can_fly = False
+# can is has
+# is_cat
+# is_dog
+# has_pen
+print(name)
+print(age)
+print(weight)
+print(can_walk)
+print(can_fly)
---
+++ variable/gettype.py
@@ -0,0 +1,10 @@
+name = "Atitaya Arsawang"
+age = 20
+weight = 80.0
+can_walk = True
+can_fly = False
+print(type(name))
+print(type(age))
+print(type(weight))
+print(type(can_walk))
+print(type(can_fly))
