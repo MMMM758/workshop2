63f70f6e033857fd6de49bd6bb199041d2f5578b
MMMM758 <77188725+MMMM758@users.noreply.github.com>
2021-01-16 14:30:48 +0700

    add string to file

---
+++ String/f-strings.py
@@ -0,0 +1,4 @@
+name = "Mew"
+age = 20
+result = f"My name is {name}, and I am {age}"
+print("result", result)  # Output : result My name is Mew, and I am 20
---
+++ String/stringformat.py
@@ -0,0 +1,5 @@
+age = 20
+txt = "My name is Mew, and I am {} "
+
+result = txt.format(age)
+print("Result", result)  # Output : Result My name is Mew, and I am 20
